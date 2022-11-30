d48e76d3e5feddf34fdac32518c844bff0a7a71c
NabilaSavitri <nabilasavitri1206@mhs.mdp.ac.id>
2022-11-30 17:02:40 +0700

    lanjutan

---
+++ app/src/main/java/com/si5a/myquote/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.si5a.myquote.Model;
+
+public class QuoteModel {
+    private String text, Author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+}
